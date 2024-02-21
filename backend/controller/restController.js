ea78652c18ef04d898934daafb5db6b534a2d4f0
Amine-Selmane <selmaneamine1@gmail.com>
2024-02-21 17:20:21 +0100

    first commit

--- backend/controller/restController.js
+++ backend/controller/restController.js
@@ -1,6 +1,6 @@
 const asyncHandler = require ("express-async-handler")
 
-const myModel = require ("../model/myModel") //nononon
+const myModel = require ("../model/myModel")
 
 //Get 
 //@Route  Get /path/path
