177b5f4ae8d11b976591e1a2812cf3689a7050fc
Amine-Selmane <selmaneamine1@gmail.com>
2024-02-21 17:05:03 +0100

    first commit

---
+++ backend/model/myModel.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose')
+
+const myModelSchema = new mongoose.Schema({
+    
+})
+
+module.exports = mongoose.model('myModel', myModelSchema)
---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require ("express")
+const colors = require('colors')
+const dotenv = require ("dotenv").config()
+const connectDB = require("./config/db")
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use('/path', require('./routes/restRoutes')) uncomment and change the path depending on yours // require stays like that
+ 
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
