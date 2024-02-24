d233fa8f37fee2d0e172af609c84c1b3ce3f3757
kinzamestiri <kinza.mestiri@esprit.tn>
2024-02-25 00:00:26 +0100

    hide info

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,3 @@
 module.exports ={
-    JWT_SECRET : "xudqZctRgaubguwVKFhnj9rPtIJfPmGf5LSBumjrI8wDiZfHdvf09I/kfxGWa7aMVMMXbuNNw4vmIFTLGVtgpw==",
-    EMAIL:"Kinza.Mestiri@esprit.tn",
-    PASSWORD: "212JFt1245"
+
 }
