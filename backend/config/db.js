62b69057248de3fbb3d1c0fb5d7da571e0dbdc74
manar el kefi <manar.elkefi@esprit.tn>
2024-02-28 10:20:28 +0100

    second version

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,30 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectDB = async() =>{
+const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-    } catch (error){
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true // Ajoutez cette option si nécessaire
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+    } catch (error) {
         console.error(error);
-        process.exit(1)
+        process.exit(1);
     }
 }
 
-module.exports = connectDB
+module.exports = connectDB;
+
+// const mongoose = require('mongoose')
+
+// const connectDB = async() =>{
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI)
+//         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+//     } catch (error){
+//         console.error(error);
+//         process.exit(1)
+//     }
+// }
+
+// module.exports = connectDB
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 const express = require ("express")
+const cors = require('cors');
 const colors = require('colors')
 const dotenv = require ("dotenv").config()
 const connectDB = require("./config/db")
 const port = process.env.PORT || 5000 // 5000 is the default
-
+const reportRouter = require("./routes/reports");
 connectDB()
 
 const app = express()
-
+app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // app.use('/path', require('./routes/restRoutes')) uncomment and change the path depending on yours // require stays like that
- 
+app.use("/reports", reportRouter);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
