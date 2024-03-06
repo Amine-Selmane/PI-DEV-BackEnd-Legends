f64c3c7412215e5991c0ca8d5e0c329c380ff55d
Amine-Selmane <selmaneamine1@gmail.com>
2024-03-06 15:47:22 +0100

    Duplicates fixed, server runs

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async() =>{
     try {
-        const conn1 = await mongoose.connect(process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI_SRV)
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-        console.log(`MongoDB Connected: ${conn1.connection.host}`.cyan.underline)
     } catch (error){
         console.error(error);
         process.exit(1)
